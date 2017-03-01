5614f616aaab7ec663c1281d937c6b7d89d1ed92
Mariam Mazen <mariammazen@paymobsolutions.com>
2017-03-01 15:46:22 +0200

    Added Chat Layout

--- app/src/main/java/com/iot/guc/jarvis/MainActivity.java
+++ app/src/main/java/com/iot/guc/jarvis/MainActivity.java
@@ -1,13 +1,29 @@
 package com.iot.guc.jarvis;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends FragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            ChatFragment chatfragment = new ChatFragment();
+
+            chatfragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, chatfragment).commit();
+        }
     }
+
 }
+
