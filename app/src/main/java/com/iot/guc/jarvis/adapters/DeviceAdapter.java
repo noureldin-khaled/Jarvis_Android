ce0ed39bb3d1cec6c9b40ad2894ba51d2ec8ec23
Nour ElDin Khaled <mastergame_2007@hotmail.com>
2017-04-05 16:43:43 +0200

    finished switch actions and app icon

--- app/src/main/java/com/iot/guc/jarvis/adapters/DeviceAdapter.java
+++ app/src/main/java/com/iot/guc/jarvis/adapters/DeviceAdapter.java
@@ -1,26 +1,32 @@
 package com.iot.guc.jarvis.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import com.iot.guc.jarvis.R;
+import com.iot.guc.jarvis.fragments.DeviceFragment;
 import com.iot.guc.jarvis.models.Device;
+import com.iot.guc.jarvis.responses.DeviceResponse;
 
 import java.util.ArrayList;
 
 public class DeviceAdapter extends BaseAdapter {
     private ArrayList<Device> devices;
     private Context context;
+    private DeviceFragment fragment;
 
-    public DeviceAdapter(Context context, ArrayList<Device> devices) {
+    public DeviceAdapter(Context context, ArrayList<Device> devices, DeviceFragment fragment) {
         this.devices = devices;
         this.context = context;
+        this.fragment = fragment;
     }
 
     @Override
@@ -39,7 +45,7 @@ public class DeviceAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         Device device = devices.get(position);
         if (convertView == null)
             convertView = LayoutInflater.from(context).inflate(R.layout.list_item_device, parent, false);
@@ -51,8 +57,27 @@ public class DeviceAdapter extends BaseAdapter {
         TextView DeviceListItem_TextView_DeviceName = (TextView) convertView.findViewById(R.id.DeviceListItem_TextView_DeviceName);
         DeviceListItem_TextView_DeviceName.setText(device.getName());
 
-        Switch DeviceListItem_Switch_Toggle = (Switch) convertView.findViewById(R.id.DeviceListItem_Switch_Toggle);
+        final Switch DeviceListItem_Switch_Toggle = (Switch) convertView.findViewById(R.id.DeviceListItem_Switch_Toggle);
         DeviceListItem_Switch_Toggle.setChecked(device.isStatus());
+
+        DeviceListItem_Switch_Toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
+                DeviceListItem_Switch_Toggle.setEnabled(false);
+                fragment.handleDevice(position, isChecked, new DeviceResponse() {
+                    @Override
+                    public void onSuccess() {
+                        DeviceListItem_Switch_Toggle.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onFailure() {
+                        DeviceListItem_Switch_Toggle.setEnabled(true);
+                        DeviceListItem_Switch_Toggle.setChecked(!isChecked);
+                    }
+                });
+            }
+        });
 
         return convertView;
     }
